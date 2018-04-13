d85aba6eff5e0583b3e4e081db28ca5dde142591
mudasar187 <ahmmud16@hotmail.com>
2018-04-13 19:35:34 +0200

    setting up project, init env file and loaded npm module for firestore

---
+++ src/env/env.ts
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyDGuHh70fS63vUjh7UYMg41NRjVgwd0lGM",
+  authDomain: "book-sales-student.firebaseapp.com",
+  databaseURL: "https://book-sales-student.firebaseio.com",
+  projectId: "book-sales-student",
+  storageBucket: "book-sales-student.appspot.com",
+  messagingSenderId: "738520725233"
+};
