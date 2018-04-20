24eb6d40d80aecb9218d6a9e79756413ada3207b
mudasar187 <ahmmud16@hotmail.com>
2018-04-20 16:57:21 +0200

    added splashscreen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,20 @@ import { HomePage } from '../pages/home/home';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+
+  splash = true; // Set true for the splash screen, if you want to turn it off, set false
+  rootPage:any;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+
+
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      setTimeout(() => {
+        this.splash = false; // If set to true, splasscreen wil go on loop and never stop
+      }, 3400);
       statusBar.styleDefault();
       splashScreen.hide();
     });
