ff53b816e57739ec22204915866c6a94cd915557
mudasar187 <ahmmud16@hotmail.com>
2018-04-24 13:19:49 +0200

    fixed alert popup if profile not uploaded

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, AlertCmp, AlertController } from 'ionic-angular';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { LoginPage } from '../login/login';
 import { User } from '../../models/User';
@@ -21,7 +21,9 @@ export class ProfilePage {
   user = {} as User; // create an object of user
   userObject: any;
   profileImage: string;
-  deleteImage: string;
+  loadingPopUp: any;
+  alertPopUp: any;
+
 
   constructor(
     public navCtrl: NavController,
@@ -29,12 +31,13 @@ export class ProfilePage {
     private af: AngularFirestore,
     private camera: Camera,
     private afStorage: AngularFireStorage,
-    private photoViewer: PhotoViewer) {
+    private photoViewer: PhotoViewer,
+    private loader: LoadingController,
+    private alert: AlertController) {
 
     this.userObject = this.af.app.auth().currentUser;
     this.user.nickname = this.userObject.displayName;
     this.user.email = this.userObject.email;
-    this.deleteImage = this.userObject.photoURL;
   }
 
   resizeImage() {
@@ -55,9 +58,32 @@ export class ProfilePage {
     })
   }
 
+  // present loader for chaning profil picture, to show user that something is happening if connection is slow
+  presentLoader(title: string) {
+      this.loadingPopUp = this.loader.create({
+        spinner: "bubbles",
+        content: title
+      });
+      this.loadingPopUp.present();
+  }
+
+  // dismiss loader when profile picture is updated
+  dismissLoader() {
+    this.loadingPopUp.dismiss();
+  }
+
+  // present alert if profile picture cannot be changed, specify that something is wrong, connection or firebase
+  presentAlert(title: string) {
+    let alert = this.alert.create({
+      title: title,
+      buttons: ['OK'],
+      enableBackdropDismiss: false // make user not allowed to press outside, must press ok
+    });
+    alert.present();
+  }
+
   // to add profile picture to firestorage
   addProfilePicture() {
-
     // generate a filename for the image we're going to upload based on user's email and second
     let imageFileName = `${this.af.app.auth().currentUser.email}_${new Date().getTime()}.png`;
 
@@ -71,13 +97,18 @@ export class ProfilePage {
     let uploadEvent = task.downloadURL();
 
     // when the image is uploaded, we can now get the URL accsess
+    this.presentLoader('Oppdaterer profilbildet'); // show user that picture is being updated
     uploadEvent.subscribe((uploadImgUrl) => {
       this.af.app.auth().currentUser.updateProfile({ displayName: this.user.nickname, photoURL: uploadImgUrl });
+      this.dismissLoader(); // dismiss when its updated
+    }, (err : any) => {
+      this.dismissLoader();
+      this.presentAlert('Oppss.. Noe gikk galt..');
     });
   }
 
-  deleteLastProfileImage() {
-
+  deleteLastProfilePicture(lastImageURL: string) {
+    // delete last previous image
   }
 
   // logout from app
