cbc61c8b78cc2cffa9aa9608390df8d12ac2b980
mudasar187 <ahmmud16@hotmail.com>
2018-04-25 12:11:11 +0200

    added book interface

---
+++ src/models/Book.ts
@@ -0,0 +1,11 @@
+export interface Book {
+  userUid: string,
+  nicknameOwner: string,
+  bookImage: string,
+  bookTitle: string,
+  price: number,
+  location: string,
+  aboutBookText: string,
+  sold: false
+  conditions: string
+}
