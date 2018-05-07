336b1fae3bd6fd9466d19560708e14fbd06747d8
mudasar187 <ahmmud16@hotmail.com>
2018-05-07 15:02:53 +0200

    almost done with detailpage

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -1,6 +1,8 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Book } from '../../models/Book';
+import { PhotoOptions } from '../../cameraOptions/PhotoOptions';
+import { PhotoViewer } from '@ionic-native/photo-viewer';
 
 declare var google: any;
 
@@ -17,12 +19,20 @@ export class DetailPage {
 
   @ViewChild('map') mapRef: ElementRef;
 
+  private photoOptions: PhotoOptions; // create an object of type PhotoOptions
   private map: any; // create a variable to hold the map
   book: Book; // create an object of type Book
 
   constructor(private navCtrl: NavController,
-    private navParams: NavParams) {
+    private navParams: NavParams,
+    private photoViewer: PhotoViewer,) {
+      this.photoOptions = new PhotoOptions(this.photoViewer, null); // a new instance of PhotoOptions
     this.book = navParams.get('book'); // get the specific book from BuyBuyPage
+  }
+
+  // make picture bigger when user click on profile picture
+  makeImageBigger(book: Book) {
+    this.photoOptions.resizeImage(book.bookImage); // take the image url string in parameter
   }
 
   // initialize the map with lat and lng coordinates
